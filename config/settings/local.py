aecddbc36c89ddfed94b276ff9924dbf731f5f43
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2024-07-29 17:26:58 +0700

    tested with portainer docker desktop

---
+++ config/settings/local.py
@@ -0,0 +1,49 @@
+from os import getenv, path
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+from .base import *  # noqa
+from .base import BASE_DIR
+
+local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
+
+if path.isfile(local_env_file):
+    load_dotenv(local_env_file)
+
+DEBUG = True
+
+SITE_NAME = getenv("SITE_NAME")
+
+SECRET_KEY = getenv(
+    "DJANGO_SECRET_KEY", 
+    "GXmCVLxfOCDK3iU38klf7YNnu2Q3ViIz1ZMwcj0uPj_khQ3VqSo"
+    )
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+ADMIN_URL = getenv("DJANGO_ADMIN_URL")
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = getenv("EMAIL_HOST")
+EMAIL_PORT = getenv("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
+DOMAIN = getenv("DOMAIN")
+
+LOGGING = {"version": 1, "disable_existing_loggers": False,
+           "formatters": {
+               "verbose": {"format": "%(levelname)s %(name)-12s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"},
+           },
+           "handlers":{
+               "console": {
+                   "level": "DEBUG",
+                   "class": "logging.StreamHandler",
+                   "formatter": "verbose"
+               },
+           },
+           "root": {
+               "level": "INFO",
+               "handlers": [
+                   "console"
+               ]
+           }
+           }
---
+++ staticfiles.md
@@ -0,0 +1,4 @@
+```bash
+mkdir -m 777 staticfiles
+```
+this will make a directory where docker have full access to it
